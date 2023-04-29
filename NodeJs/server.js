f4ecc71d55f69a3c53df1ada418fdae885333bd0
Abul Khair Rifat <110252350+avoidrifat@users.noreply.github.com>
2023-04-29 14:31:07 +0600

    Node JS Assignment

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,39 +1,63 @@
-const fs = require('fs');
 const http = require('http');
-const path = require('path');
+const fs = require('fs');
 
-const PUBLIC_DIR = path.join(__dirname, 'Public');
-
-var server = http.createServer(function (req, res) {
-  var filePath = path.join(PUBLIC_DIR, req.url);
-
-  if (fs.statSync(filePath).isDirectory()) {
-    filePath+='/index.html';
-  }
-
-  fs.readFile(filePath, function (error, data) {
+const server = http.createServer(function(req,res){
+if (req.url==="/" || req.url==="/home") {
+  fs.readFile("./Public/home.html" , function(error,data){
     if (error) {
       res.writeHead(404, { "content-type": "text/html" });
-      res.write('404 Not Found');
+      res.write("404 Not Found");
       res.end();
     } else {
-      let contentType = 'text/html';
-      const extname = path.extname(filePath);
-      switch (extname) {
-        case '.css':
-          contentType = 'text/css';
-          break;
-        case '.js':
-          contentType = 'text/javascript';
-          break;
-      }
-
-      res.writeHead(200, { "content-type": contentType });
+      res.writeHead(200, { "content-type": "text/html" });
       res.write(data);
       res.end();
     }
-  });
-});
-
+  })
+} else if(req.url==="/about"){
+  fs.readFile("./Public/about.html" , function(error,data){
+    if (error) {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("404 Not Found");
+      res.end();
+    } else {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  })
+}
+else if(req.url==="/contact"){
+  fs.readFile("./Public/contact.html" , function(error,data){
+    if (error) {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("404 Not Found");
+      res.end();
+    } else {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  })
+}
+else if(req.url==="/service"){
+  fs.readFile("./Public/service.html" , function(error,data){
+    if (error) {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("404 Not Found");
+      res.end();
+    } else {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  })
+}
+else{
+  res.writeHead(404, { "content-type": "text/html" });
+  res.write("<h1 style='font-size: 3em; text-align: center;'>404 Not Found</h1>");
+  res.end();
+}
+})
 server.listen(3000);
-console.log("Server is successfully running.");
+console.log("server running...");
