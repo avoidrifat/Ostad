680f036141e586e76ff4bf9c13444052c6939497
Abul Khair Rifat <110252350+avoidrifat@users.noreply.github.com>
2023-06-09 19:07:41 +0600

    Final Submission

---
+++ Module10/server/middlewares/jwt.middleware.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+exports.authenticate = (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    const secretKey = "avoidrifat";
+    const decoded = jwt.verify(token, secretKey);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized error." });
+  }
+};
---
+++ Module10/server/server.js
@@ -0,0 +1,18 @@
+const { PORT, DATA_BASE, app, mongoose } = require("./app");
+
+// connect mongoose....
+const mongooseConnect = async () => {
+  try {
+    await mongoose.connect(DATA_BASE);
+    console.log("mongoose connect done..");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// server run ...
+
+app.listen(PORT, async () => {
+  console.log(`server run successful http://127.0.0.1:${PORT}`);
+  await mongooseConnect();
+});
